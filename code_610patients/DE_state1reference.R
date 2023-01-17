856d59ef267a2779645069793399cd7a21a8e16a
lauraheath <lmheath5@gmail.com>
2023-01-17 13:53:07 -0800

    Add files via upload

---
+++ code_610patients/DE_state1reference.R
@@ -0,0 +1,138 @@
+######### branch-specific differential expression analysis
+
+#need Monrun object from prot_lineage_monocle_rerun.R and gene_short_name vector
+#script for males following script for females (5 states in males, 8 in females)
+#upload entire matrix first to look at associations with ALL genes, not just DE genes included for trajectory model
+
+MonRun <- readRDS(file="~/prot-lineage/data_objects/MonRun_prot_female.RDS")
+temp <- readRDS(file="~/prot-lineage/data_objects/Female_prot_matrix.rds")
+gene_short_name <- rownames(temp)
+table(MonRun$State2)
+#there are 6 states in the female tree
+#pre-process data for ANOVA test
+l2 <- list()
+l2$gene_names <- gene_short_name
+l2$p_2 <- rep(0,length(gene_short_name))
+l2$p_3 <- rep(0,length(gene_short_name))
+l2$p_4 <- rep(0,length(gene_short_name))
+l2$p_5 <- rep(0,length(gene_short_name))
+l2$p_6 <- rep(0,length(gene_short_name))
+l2$p_7 <- rep(0,length(gene_short_name))
+l2$p_8 <- rep(0,length(gene_short_name))
+
+l2$d_2 <- rep(0,length(gene_short_name))
+l2$d_3 <- rep(0,length(gene_short_name))
+l2$d_4 <- rep(0,length(gene_short_name))
+l2$d_5 <- rep(0,length(gene_short_name))
+l2$d_6 <- rep(0,length(gene_short_name))
+l2$d_7 <- rep(0,length(gene_short_name))
+l2$d_8 <- rep(0,length(gene_short_name))
+
+for (i in 1:length(gene_short_name)){
+  l <- list()
+  l$x <- as.vector(t(temp[i,]))
+  l$s <- as.character(MonRun$State2)
+  
+  df <- as.data.frame(l)
+  res.aov <- aov(x ~ s, data = df)
+  tk <- TukeyHSD(res.aov)
+  
+  l2$p_2[i] <- tk$s[1,4]
+  l2$p_3[i] <- tk$s[2,4]
+  l2$p_4[i] <- tk$s[3,4]
+  l2$p_5[i] <- tk$s[4,4]
+  l2$p_6[i] <- tk$s[5,4]
+  l2$p_7[i] <- tk$s[6,4]
+  l2$p_8[i] <- tk$s[7,4]
+  
+  l2$d_2[i] <- tk$s[1,1]
+  l2$d_3[i] <- tk$s[2,1]
+  l2$d_4[i] <- tk$s[3,1]
+  l2$d_5[i] <- tk$s[4,1]
+  l2$d_6[i] <- tk$s[5,1]
+  l2$d_7[i] <- tk$s[6,1]
+  l2$d_8[i] <- tk$s[7,1]
+}
+
+#save the data
+df2 <- as.data.frame(l2)
+dfa <- dplyr::select(df2,gene_names,dplyr::starts_with('p'))
+dfb <- dplyr::select(df2,gene_names,dplyr::starts_with('d'))
+
+dfa1 <- tidyr::gather(dfa,'state','pvalue',-gene_names)
+dfb1 <- tidyr::gather(dfb,'state','effect',-gene_names)
+
+dfa1$state <- sapply(dfa1$state,function(x) strsplit(x,'p_')[[1]][2])
+dfb1$state <- sapply(dfb1$state,function(x) strsplit(x,'d_')[[1]][2])
+
+df3 <- dplyr::left_join(dfa1,dfb1)
+df3$gene_short_name <- gsub("\\|.*","",df3$gene_names)
+write.csv(df3,file='~/prot-lineage/data_objects/female_DEanova_statsALLGENES.csv',quote=F,row.names=F)
+file <- synapser::File(path='~/prot-lineage/data_objects/female_DEanova_statsALLGENES.csv', parentId='syn25607662')
+file <- synapser::synStore(file)
+
+
+
+
+
+#run Male monocle object with script below:
+MonRun <- readRDS(file="~/prot-lineage/data_objects/MonRun_prot_male.RDS")
+temp <- readRDS(file="~/prot-lineage/data_objects/Male_prot_matrix.rds")
+gene_short_name <- rownames(temp)
+table(MonRun$State2)
+
+MonRun$State2<-as.character(MonRun$State2)
+MonRun$State2<-as.factor(MonRun$State2)
+#for males there are 5 states
+#pre-process data for ANOVA test
+l2 <- list()
+l2$gene_names <- gene_short_name
+l2$p_2 <- rep(0,length(gene_short_name))
+l2$p_3 <- rep(0,length(gene_short_name))
+l2$p_4 <- rep(0,length(gene_short_name))
+l2$p_5 <- rep(0,length(gene_short_name))
+
+l2$d_2 <- rep(0,length(gene_short_name))
+l2$d_3 <- rep(0,length(gene_short_name))
+l2$d_4 <- rep(0,length(gene_short_name))
+l2$d_5 <- rep(0,length(gene_short_name))
+
+
+for (i in 1:length(gene_short_name)){
+  l <- list()
+  l$x <- as.vector(t(temp[i,]))
+  l$s <- as.character(MonRun$State2)
+  
+  df <- as.data.frame(l)
+  res.aov <- aov(x ~ s, data = df)
+  tk <- TukeyHSD(res.aov)
+  
+  l2$p_2[i] <- tk$s[1,4]
+  l2$p_3[i] <- tk$s[2,4]
+  l2$p_4[i] <- tk$s[3,4]
+  l2$p_5[i] <- tk$s[4,4]
+  
+  l2$d_2[i] <- tk$s[1,1]
+  l2$d_3[i] <- tk$s[2,1]
+  l2$d_4[i] <- tk$s[3,1]
+  l2$d_5[i] <- tk$s[4,1]
+}
+
+#save the data
+df2 <- as.data.frame(l2)
+dfa <- dplyr::select(df2,gene_names,dplyr::starts_with('p'))
+dfb <- dplyr::select(df2,gene_names,dplyr::starts_with('d'))
+
+dfa1 <- tidyr::gather(dfa,'state','pvalue',-gene_names)
+dfb1 <- tidyr::gather(dfb,'state','effect',-gene_names)
+
+dfa1$state <- sapply(dfa1$state,function(x) strsplit(x,'p_')[[1]][2])
+dfb1$state <- sapply(dfb1$state,function(x) strsplit(x,'d_')[[1]][2])
+
+df3 <- dplyr::left_join(dfa1,dfb1)
+df3$gene_short_name <- gsub("\\|.*","",df3$gene_names)
+
+write.csv(df3,file='~/prot-lineage/data_objects/male_DEanova_statsALLGENES.csv',quote=F,row.names=F)
+file <- synapser::File(path='~/prot-lineage/data_objects/male_DEanova_statsALLGENES.csv', parentId='syn25607662')
+file <- synapser::synStore(file)
+
